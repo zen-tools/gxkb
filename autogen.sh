4df86fae786a5f012f5cc91c26d7f7eed096dab9
ZEN <admin@linuxhub.ru>
2016-12-01 18:10:22 +0200

    Check if automake and autoconf are installed

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
-echo "If you get errors, you may need newer versions of automake and autoconf."
-echo "You'll need at least automake 1.5 and autoconf 2.50."
+
+test -n "$(which automake)" || {
+    echo "[ERROR] automake is missing on your system" 1>&2;
+    exit 1;
+}
+
+test -n "$(which autoconf)" || {
+    echo "[ERROR] autoconf is missing on your system" 1>&2;
+    exit 2;
+}
+
+echo "If you get errors, you may need newer versions of automake and autoconf." 2>&1;
+echo "You'll need at least automake 1.5 and autoconf 2.50." 2>&1;
 
 aclocal $ACLOCAL_FLAGS \
 && autoheader \
 && automake --add-missing --copy \
-&& autoconf
+&& autoconf \
+&& echo "Now you are ready to run ./configure";
 
-echo "Now you are ready to run ./configure"
+exit 0;
+
