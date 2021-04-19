zen-tools/gxkb

commit d70f5d43867541a592c85dd0327e0115ff0dedfb

Return flag icon path for language variant if image exist

src/xkb-util.c
@@ -24,42 +24,40 @@
 #include "xkb-util.h"
 
 gchar*
-xkb_util_get_flag_filename( const gchar* group_name )
+xkb_util_get_flag_filename( const gchar* group_name, const gchar *variant )
 {
-    gchar* filepath;
-
     if( group_name == NULL )
         return NULL;
 
-    gchar *flag_name = g_ascii_strdown( group_name, -1 );
-    flag_name = g_strstrip( flag_name );
-
-    gchar* filename = g_strconcat( flag_name, ".png", NULL );
-    g_free( flag_name );
-
-    // Try to get image from user data directory
-    filepath = g_strjoin( "/", xkb_util_get_data_dir(),
-                          "flags", filename, NULL );
-
-    if( g_file_test( filepath, G_FILE_TEST_EXISTS ) )
+    GSList *flag_names = NULL;
+    if(variant && strlen( variant ) > 0)
     {
-        g_free( filename );
-        return filepath;
+        flag_names = g_slist_prepend(
+            flag_names,
+            g_strjoin(
+                "_", g_ascii_strdown( group_name, -1 ),
+                g_ascii_strdown( variant, -1 ), NULL
+            )
+        );
     }
+    flag_names = g_slist_prepend( flag_names, g_ascii_strdown( group_name, -1 ) );
+    flag_names = g_slist_prepend( flag_names, g_strdup("zz") );
+    flag_names = g_slist_reverse( flag_names );
 
-    // Try to get image from system directory
-    filepath = g_strjoin( "/", FLAGSDIR, filename, NULL );
+    gchar* file_path = NULL;
+    do {
+        gchar* flag_name = flag_names->data;
+        gchar* file_name = g_strconcat( flag_name, ".png", NULL );
+        file_path = g_strjoin( "/", xkb_util_get_data_dir(),
+                              "flags", file_name, NULL );
+        g_free( file_name );
 
-    g_free( filename );
+        if( g_file_test( file_path, G_FILE_TEST_EXISTS ) )
+            break;
+    } while( (flag_names = g_slist_next( flag_names )) );
 
-    if( !g_file_test( filepath, G_FILE_TEST_EXISTS ) )
-    {
-        filepath = g_strjoin( "/", FLAGSDIR, "zz.png", NULL );
-        if( !g_file_test( filepath, G_FILE_TEST_EXISTS ) )
-            return NULL;
-    }
-
-    return filepath;
+    g_slist_free( flag_names );
+    return file_path;
 }
 
 gchar*

data/flags/Makefile.am
@@ -4,7 +4,6 @@ images_DATA =        \
     bg.png           \
     by.png           \
     cz.png           \
-    cz_qwerty.png    \
     de.png           \
     ee.png           \
     gb.png           \

src/statusicon.c
@@ -128,7 +128,8 @@ void
 statusicon_update_current_image( void )
 {
     const gchar *group_name = xkb_config_get_group_name( -1 );
-    gchar *filepath = xkb_util_get_flag_filename( group_name );
+    const gchar *variant = xkb_config_get_variant( -1 );
+    gchar *filepath = xkb_util_get_flag_filename( group_name, variant );
 
     if( icon_type == SYSTRAY )
     {
@@ -176,7 +177,8 @@ statusicon_update_menu( void )
     {
         gchar *layout_string = NULL;
 
-        gchar *imgfilename = xkb_util_get_flag_filename( xkb_config_get_group_name( i ) );
+        gchar *imgfilename = xkb_util_get_flag_filename( xkb_config_get_group_name( i ),
+                                                         xkb_config_get_variant( i ) );
         GdkPixbuf *handle = gdk_pixbuf_new_from_file_at_scale( imgfilename, width, height, TRUE, NULL );
         g_free( imgfilename );
 

src/xkb-util.h
@@ -28,7 +28,8 @@
 
 #include <glib.h>
 
-gchar*      xkb_util_get_flag_filename      (const gchar* group_name);
+gchar*      xkb_util_get_flag_filename      (const gchar* group_name,
+                                             const gchar *variant);
 
 gchar*      xkb_util_get_layout_string      (const gchar* group_name,
                                              const gchar* variant);

commit fb778f80f44d977ea973caee8fc9f610438abf0e

Fix regression caused by d70f5d4

src/xkb-util.c
@@ -29,7 +29,7 @@ xkb_util_get_flag_filename( const gchar* group_name, const gchar *variant )
     if( group_name == NULL )
         return NULL;
 
-    GSList *flag_names = NULL;
+    GSList* flag_names = NULL;
     if(variant && strlen( variant ) > 0)
     {
         flag_names = g_slist_prepend(
@@ -41,19 +41,32 @@ xkb_util_get_flag_filename( const gchar* group_name, const gchar *variant )
         );
     }
     flag_names = g_slist_prepend( flag_names, g_ascii_strdown( group_name, -1 ) );
-    flag_names = g_slist_prepend( flag_names, g_strdup("zz") );
+    flag_names = g_slist_prepend( flag_names, "zz" );
     flag_names = g_slist_reverse( flag_names );
 
     gchar* file_path = NULL;
     do {
-        gchar* flag_name = flag_names->data;
-        gchar* file_name = g_strconcat( flag_name, ".png", NULL );
-        file_path = g_strjoin( "/", xkb_util_get_data_dir(),
-                              "flags", file_name, NULL );
-        g_free( file_name );
+        gchar* file_name = g_strconcat( flag_names->data, ".png", NULL );
 
-        if( g_file_test( file_path, G_FILE_TEST_EXISTS ) )
+        // Try to get image from user data directory
+        gchar* data_dir = xkb_util_get_data_dir();
+        file_path = g_strjoin( "/", data_dir, "flags", file_name, NULL );
+        g_free( data_dir );
+        if( g_file_test( file_path, G_FILE_TEST_EXISTS ) ) {
+            g_free( file_name );
             break;
+        }
+        g_free( file_path );
+
+        // Try to get image from system directory
+        file_path = g_strjoin( "/", FLAGSDIR, file_name, NULL );
+        if( g_file_test( file_path, G_FILE_TEST_EXISTS ) ) {
+            g_free( file_name );
+            break;
+        }
+        g_free( file_path );
+
+        g_free( file_name );
     } while( (flag_names = g_slist_next( flag_names )) );
 
     g_slist_free( flag_names );